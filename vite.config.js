d94e270428e8407df7c45d16962798141d9d9ef1
Hosea <karanja.mungai23@gmail.com>
2025-03-18 09:59:52 +0300

    allowed render host

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ["ims-zijf.onrender.com"],
     host: true,
     strictPort: true,
     port: 8000,
     },
-    
+
 })
