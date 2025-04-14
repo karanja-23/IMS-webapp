7db1987b64ee062f3c391684b9d61411957a1d02
Hosea <karanja.mungai23@gmail.com>
2025-04-14 23:06:16 +0300

    modifies profile page

--- src/pages/orders.jsx
+++ src/pages/orders.jsx
@@ -1,26 +1,88 @@
 import SidebarComponent from "../components/sidebar";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../context/context";
-import CircleNotificationsRoundedIcon from '@mui/icons-material/CircleNotificationsRounded';
+import CircleNotificationsRoundedIcon from "@mui/icons-material/CircleNotificationsRounded";
+import Notification from "../components/notification";
+import { ToastContainer } from "react-toastify";
 function Orders() {
-    const{isOpen} = useContext(AppContext)
-    return (
-    <div className="main">           
-        <SidebarComponent />
-        <div className="content" style={{boxSizing:'border-box',width: isOpen ? 'calc(100vw - 210px)' : 'calc(100vw - 70px)',left: isOpen ? 202 : 62, transition: '0.3s'}} >
-            <div className="header">
-                <div className="title">                
-                    <h2>Orders</h2>
-                    
-                </div>
-                <div className="notification">
-                    <CircleNotificationsRoundedIcon style={{marginRight: '10px', fontSize: '1.9rem'}}/>
-                    
-                </div>
+  const { isOpen, loggedIn, setAccessToken, setLoggedIn } =
+    useContext(AppContext);
+  const [orders, setOrders] = useState([]);
+  const [loadingOrderDetais, setLoadingOrderDetails] = useState(false);
+  useEffect(() => {
+    if (loggedIn) {
+      const ordersInAlphabeticalOrder = JSON.parse(
+        localStorage.getItem("orders")
+      )?.sort((a, b) => a.order_id.localeCompare(b.order_id));
 
+      setOrders(ordersInAlphabeticalOrder);
+    } else {
+      const token = localStorage.getItem("token");
+
+      if (token) {
+        setAccessToken(token);
+        fetch(`https://mobileimsbackend.onrender.com/protected/user`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            if (data["msg"] === "Token has expired") {
+              localStorage.removeItem("token");
+              setAccessToken("");
+              setLoggedIn(false);
+              navigate("/login");
+            }
+            if (data) {
+              setUser(data);
+              setLoggedIn(true);
+              setIsLoading(false);
+            }
+          })
+          .then(() => {
+            if (orders.length === 0) {
+              gettOrders();
+            }
+          });
+      } else {
+        navigate("/login");
+      }
+    }
+  }, []);
+  async function gettOrders() {}
+  return (
+    <>
+      <div className="main">
+        <SidebarComponent />
+        <ToastContainer />
+        <div
+          className="content"
+          style={{
+            boxSizing: "border-box",
+            width: isOpen ? "calc(100vw - 210px)" : "calc(100vw - 70px)",
+            left: isOpen ? 202 : 62,
+            transition: "0.3s",
+          }}
+        >
+          {loadingOrderDetais ? <Loading /> : null}
+          <div
+            className="header"
+            style={{
+              width: isOpen ? "calc(100vw - 210px)" : "calc(100vw - 70px)",
+              transition: "0.3s",
+            }}
+          >
+            <div className="title">
+              <h2>Orders</h2>
             </div>
-        </div>       
-    </div>
-    )
+            <Notification />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-export default Orders
+export default Orders;
